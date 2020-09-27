6909441d929598ccf1438c85d4dcef48f57980b6
Lexie Mangune <alexandra_cyrielle_mangune@dlsu.edu.ph>
2020-09-27 20:31:26 +0800

    merge issue resolved

--- Client.java
+++ Client.java
@@ -3,20 +3,18 @@ import java.awt.*;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
->>>>>>> 933745baa5f5d853fccbeff7122d0e914b03c4ae
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
 
-    /* BACK-END components **/
     ClientConnection connection;
     Scanner sc = new Scanner(System.in);
     String name;
     String host;
     int port;
 
-<<<<<<< HEAD
+
     // main chat area
     JFrame frame = new JFrame("De La Salle Usap");
     JPanel panel = new JPanel();
@@ -100,104 +98,8 @@ public class Client {
         } catch (Exception e) {
             e.printStackTrace();
         }
-=======
-    JTextField nameField;
-    JTextField hostField;
-    JTextField portField;
-
-    /* CLIENT constructor **/
-    public Client() {
-
-        /* FRONT-END
-           First panel - asks the user for the necessary
-           details before proceeding to the main panel
-          **/
-
-        JFrame mainFrame = new JFrame();
-
-        JPanel mainPanel = new JPanel();
-        mainPanel.setBorder(BorderFactory.createEmptyBorder(200,200,200,200));
-        mainPanel.setLayout(new GridLayout(5,1));
-
-        JLabel test1 = new JLabel("Testing main panel");
-        mainPanel.add(test1);
-
-        JFrame loginFrame = new JFrame();
-
-        JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(200,200,200,200));
-        panel.setLayout(new GridLayout(5,1));
-
-        nameField = new JTextField("Enter name");
-        nameField.setBounds(50,100, 300,50);
-
-        hostField = new JTextField("Enter IP (Host) address");
-        hostField.setBounds(50,100, 300,50);
-
-        portField = new JTextField("Enter port number");
-        portField.setBounds(50,100, 300,50);
-
-        JButton loginButton = new JButton("Join Chat");
-        loginButton.setBounds(50, 100, 300, 50);
-        loginButton.addActionListener(event -> {
-            /*
-              Inputted texts from fields are transferred
-              the the their respective variables
-              **/
-            name = nameField.getText();
-            host = hostField.getText();
-            port = Integer.parseInt(portField.getText());
-
-            /* BACK-END
-               connects a user to the server after providing
-               the necessary information
-              **/
-            try {
-                System.out.println("Name: " + name);
-                System.out.println("host: " + host);
-                System.out.println("port: " + port);
-
-                loginFrame.setVisible(false);
-                mainFrame.setVisible(true);
-
-                Socket endpoint = new Socket(host, port);
-                connection = new ClientConnection(endpoint, this);
-                connection.start();
-
-                listenForMessages();
-
-                connection.terminateConnection();
-                sc.close();
-                connection.close();
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-
-
-        panel.add(nameField);
-        panel.add(hostField);
-        panel.add(portField);
-        panel.add(loginButton);
-
-        loginFrame.add(panel, BorderLayout.CENTER);
-        loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        loginFrame.setSize(200,500);
-        loginFrame.setResizable(false);
-        loginFrame.setTitle("De La Salle Usap");
-        loginFrame.pack();
-        loginFrame.setVisible(true);
-
-        mainFrame.add(mainPanel, BorderLayout.CENTER);
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainFrame.setSize(200,500);
-        mainFrame.setResizable(false);
-        mainFrame.setTitle("De La Salle Usap");
-        mainFrame.pack();
-        mainFrame.setVisible(false);
->>>>>>> 933745baa5f5d853fccbeff7122d0e914b03c4ae
     }
+
 
     public void listenForMessages(String message) {
         this.sendMsg(message);
@@ -233,11 +135,6 @@ public class Client {
     public static void main(String[] args) {
         Client client = new Client();
         client.frame.setVisible(true);
-    }
-
-    public static void main(String[] args) {
-        new Client();
-
     }
 
 }
